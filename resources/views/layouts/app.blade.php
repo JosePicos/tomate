dbbc2dd69f886bebe9df796e1a8dce758077ce83
Jose Picos <jl.moonlight93@gmail.com>
2017-11-18 17:58:01 -0700

    Proyecto de Ingenieria Web

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    
+        <!-- Styles -->
+        <link href="{{asset('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css')}}" rel="stylesheet">
+        <link href="{{asset('https://fonts.googleapis.com/css?family=Merriweather')}}" rel="stylesheet">
+        <link href="{{asset('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js')}}" rel="stylesheet"> 
+        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" media="screen">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/menu.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/frequisicion.css') }}" rel="stylesheet">
+    </head>
+    <body>
+         <div id="app">
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="nav-xbootstrap">
+                    <ul>
+                        <li><a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a></li>
+            <!-- Right Side Of Navbar -->
+                    <ul class=" navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                        <li>
+                            <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="glyphicon glyphicon-chevron-down iconsize"></span>
+                            </a>
+                            <ul class="dropdown">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Salir
+                                    </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                            </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                        @if (Auth::check())
+                        <li><a href="javascript:void(0)">REQUISICIÓN<span class="glyphicon glyphicon-chevron-down iconsize"></span></a>
+                            <ul class="dropdown">
+                                <li><a href=" ">Alta Requisición</a></li>
+                                <li><a href=" ">Monitoreo de Requisición</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="javascript:void(0)" >Surtir Requisición<span class="glyphicon glyphicon-chevron-down iconsize"></span></a>
+                        </li>
+                        @else
+                        <li><a href=" " >PROMOCIONES<span class=" "></span></a>        
+                        </li>
+                        <li><a href="javascript:void(0)" >QUIENES SOMOS?<span class=" "></span></a>        
+                        </li>
+                        <li><a href="javascript:void(0)" >CONTACTANOS<span class="glyphicon glyphicon-chevron-down iconsize"></span></a>
+                        </li>
+                        @endif
+                    </ul>
+                </div>
+                <div class="nav-bg-xbootstrap">
+                   <div class="navbar-xbootstrap"> <span></span> <span></span> <span></span> </div>
+                   <a href="{{ route('home') }}" class="title-mobile">PYMES FACTUR-E</a>
+                </div>    
+            </nav>
+    @yield('content')
+        </div>      
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/menu.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src ="{{asset('js/java1.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/requisicion.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+@section('content')
+<div class="col-md-12 col-md-offset dark-grey">
+      <div class="panel-body">
+        <div class="container-fluid">
+        <section class="container">
+          <div class="container-page">        
+            <div class="col-md-12">
+              <h3 class="dark-grey">Registro Requisicion</h3>
+            <!-- Folio de la factura no editable -->
+            <div class="form-group col-lg-2">
+              <label>Folio</label>
+              <input type="folio" name="" class="form-control" id="" value="">
+            </div>
+            <!-- FECHA NO EDITABLE -->
+            <div class="form-group col-lg-4">
+              <label>Fecha</label>
+              <input type="feha" name="" class="form-control" id="" value="">
+            </div>
+            <div class="form-group col-lg-2">
+              <label>Pioridad</label>
+                <select type="uso" name="" class="form-control" id="" value="" >
+                  <option value="Tacos" >Normal</option>
+                  <option>Urgente</option>
+                </select>
+            </div>
+            <!-- DIRECCION -->
+            <div class="form-group col-lg-12">
+              <label>Direccion</label>
+            </div>
+            <!-- CODIGO POSTAL -->
+            <div class="form-group col-lg-2">
+              <label>Codigo Postal</label>
+              <input type="uso" name="" class="form-control" id="" value="">
+            </div>
+            <!--CALLE -->
+            <div class="form-group col-lg-2">
+              <label>Pais</label>
+              <input type="uso" name="" class="form-control" id="" value="">
+            </div>
+            <!--COLONIA -->
+            <div class="form-group col-lg-2">
+              <label>Estado</label>
+              <input type="uso" name="" class="form-control" id="" value="">
+            </div>
+            <!-- NUMERO INTERIOR -->
+            <div class="form-group col-lg-2">
+              <label>Municipio/Provincia</label>
+              <input type="uso" name="" class="form-control" id="" value="">
+            </div>
+            <!--COLONIA -->
+            <div class="form-group col-lg-6">
+              <label>Colonia</label>
+              <input type="uso" name="" class="form-control" id="" value="">
+            </div>
+            <!--LOCALIDAD -->
+            <div class="form-group col-lg-4">
+              <label>Calle</label>
+              <input type="uso" name="" class="form-control" id="" value="">
+            </div>
+            <!--ESTADO -->
+            <div class="form-group col-lg-2">
+              <label>Numero</label>
+              <input type="uso" name="" class="form-control" id="" value="">
+            </div>
+            <!-- CODIGO POSTAL -->
+            <div class="form-group col-lg-2">
+              <label>Producto</label>
+              <input type="uso" name="" class="form-control" id="" value="">
+            </div>
+             <div class="form-group col-lg-2">
+              <label></label>
+              <button type="submit" class=" form-control btn btn-primary">Buscar</button>
+            </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+ 
+</div>
+@endsection
