fd704aa740f9d6810ef38a84199c7c85891dad31
Jose Picos <jl.moonlight93@gmail.com>
2017-11-23 19:24:45 -0700

    Se termino la vista de la requisicion

---
+++ app/empleado.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class empleado extends Model
+{
+    //
+    protected $table = "empleado";
+}
---
+++ app/lotes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class lotes extends Model
+{
+    //
+    protected $table = "lotes";
+}
