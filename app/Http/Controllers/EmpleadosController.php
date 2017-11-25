5c565a08a8a30c83ae9cc31d0398e39a0e3d7711
julio <julioingsist@gmail.com>
2017-11-25 16:46:46 -0700

    se muestra catalogo de empleados

--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Empleado;
 
 class EmpleadosController extends Controller
 {
-    //
+    public function index()
+    {
+		 $empleados = Empleado::all();
+		 return view('empleados', compact('productos'));    	
+    }
 }
