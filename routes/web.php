3196cd5ccd68e578fe7fcca3a49aba28900293e0
julio <julioingsist@gmail.com>
2017-11-25 16:36:02 -0700

    se agregan rutas los empleados

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/Requisicion/Nuevo', 'RequisicionesController@index')->name('requisicion');
 
 Route::resource('/Producto', 'ProductosController');
+
+Route::resource('/Empleado', 'EmpleadosController');
