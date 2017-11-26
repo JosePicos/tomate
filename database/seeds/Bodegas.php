49dd96f659c86ea6af4edd1cdd4f0df6f5519038
julio <julioingsist@gmail.com>
2017-11-26 13:46:49 -0700

    se cambia capacidad de las bodegas

--- database/seeds/Bodegas.php
+++ database/seeds/Bodegas.php
@@ -14,10 +14,10 @@ class Bodegas extends Seeder
     {
         $fecha = new \DateTime();
         $bodegas = array(
-                        array('id' => 1, 'nombre' => 'Asia', 'capacidad' => 150, 'disponible' => 0, 'created_at' => $fecha->format('Y-m-d H:i:s'), 'updated_at' => $fecha->format('Y-m-d H:i:s')),
-                        array('id' => 2, 'nombre' => 'Europa', 'capacidad' => 100, 'disponible' => 0, 'created_at' => $fecha->format('Y-m-d H:i:s'), 'updated_at' => $fecha->format('Y-m-d H:i:s')),
-                        array('id' => 3, 'nombre' => 'Estados Unidos', 'capacidad' => 80, 'disponible' => 0, 'created_at' => $fecha->format('Y-m-d H:i:s'), 'updated_at' => $fecha->format('Y-m-d H:i:s')),
-                        array('id' => 4, 'nombre' => 'México', 'capacidad' => 50, 'disponible' => 0, 'created_at' => $fecha->format('Y-m-d H:i:s'), 'updated_at' => $fecha->format('Y-m-d H:i:s'))
+                        array('id' => 1, 'nombre' => 'Asia', 'capacidad' => 200, 'disponible' => 0, 'created_at' => $fecha->format('Y-m-d H:i:s'), 'updated_at' => $fecha->format('Y-m-d H:i:s')),
+                        array('id' => 2, 'nombre' => 'Europa', 'capacidad' => 150, 'disponible' => 0, 'created_at' => $fecha->format('Y-m-d H:i:s'), 'updated_at' => $fecha->format('Y-m-d H:i:s')),
+                        array('id' => 3, 'nombre' => 'Estados Unidos', 'capacidad' => 300, 'disponible' => 0, 'created_at' => $fecha->format('Y-m-d H:i:s'), 'updated_at' => $fecha->format('Y-m-d H:i:s')),
+                        array('id' => 4, 'nombre' => 'México', 'capacidad' => 200, 'disponible' => 0, 'created_at' => $fecha->format('Y-m-d H:i:s'), 'updated_at' => $fecha->format('Y-m-d H:i:s'))
                     );
         Bodega::insert($bodegas);
     }
