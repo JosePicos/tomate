0b65d7426808f25bf83d5fcc06d7415ce34984a2
julio <julioingsist@gmail.com>
2017-11-25 18:01:08 -0700

    se agregan funciones a modelos y controladores

--- app/Empleado.php
+++ app/Empleado.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Empleado extends Model
 {
     protected $table = 'empleados';
+
+    public static function guardar(Request $datos)
+    {
+    	$empleado = new Empleado();
+    	$empleado->save();   
+    }
+
+    public static function actualizar($id, Request $datos)
+    {
+    	$empleado = Empleado::find($id);
+    	$empleado->save();   
+    }
+
+    public static function eliminar($id)
+    {
+    	$empleado = Empleado::find($id);
+    	$empleado->delete();
+    }
 }
--- app/Http/Controllers/EmpleadosController.php
+++ app/Http/Controllers/EmpleadosController.php
@@ -13,4 +13,24 @@ class EmpleadosController extends Controller
 		 $empleados = Empleado::all();
 		 return view('empleados', compact('productos'));    	
     }
+
+    public function store(Request $datos)
+    {
+    	Empleado::guardar($datos);
+    	Session::flash('mensaje', 'Empleado guardado exitosamente');
+    	return back();
+    }
+
+    public function update($Empleado, Request $datos)
+    {
+    	Empleado::actualizar($producto, $datos);
+    	Session::flash('mensaje', 'Datos del empleado ctualizados correctamente');
+    	return back();	
+    }
+
+    public function destroy($Empleado)
+    {
+		Empleado::eliminar($producto);
+		Session::flash('mensaje', 'Empleado eliminado exitosamente');
+    }
 }
