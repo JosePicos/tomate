4b802df603e37d097994c47f26fd2e1ef8e30aed
Jose Picos <jl.moonlight93@gmail.com>
2017-11-26 13:21:00 -0700

    Se terminan las vistas de los productos con sus modals

--- resources/views/productos/nuevo.blade.php
+++ resources/views/productos/nuevo.blade.php
@@ -1,108 +1,33 @@
+  <!-- VISTA PARA LA EDICION DE LOS CATALAGOS  -->
 
-<div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <h4 class="modal-title" id="myModalLabel">Esto es un modal</h4>
-      </div>
-       <label>Folio</label>
-              <input type="folio" name="" class="form-control" id="" value="">
-            </div>
+  <div class="modal fade" id="nuevo" tabindex="-1" role="dialog" aria-labelledby="nuevo" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+          <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+          </button>
+          <h4 class="modal-title custom_align" id="Heading">NUEVO PRODUCTO</h4>
+        </div>
+        <div class="modal-body">
+          <div class="form-group"> 
             <!-- FECHA NO EDITABLE -->
-            <div class="form-group col-lg-2">
-              <label>Fecha</label>
-              <input type="feha" name="" class="form-control" id="" value="">
-            </div>
-            <!-- USO DE CFDI -->
-            <div class="form-group col-lg-2">
-              <label>Uso CFDI</label>
-                <select type="uso" name="" class="form-control" id="" value="" >
-                  <option value="Tacos" >Aquisicion de mercancia</option>
-                  <option>Gastos Medicos</option>
-                </select>
-            </div>
-            <!--FORMAR DE PAGO -->
-            <div class="form-group col-lg-2">
-              <label>Forma de Pago</label>
-                <select type="uso" name="" class="form-control" id="" value="" >
-                  <option value="Tacos" >Aquisicion de mercancia</option>
-                  <option>Gastos Medicos</option>
-                </select>
-            </div>
-            <!--CONDICION DE PAGO -->
-            <div class="form-group col-lg-2">
-              <label>Condicion de Pago</label>
-                <select type="uso" name="" class="form-control" id="" value="" >
-                <option value="Tacos" >Aquisicion de mercancia</option>
-              <option>Gastos Medicos</option>
-            </select>
-            </div>
-            <!--RAZON SOCAL O NOMBRE -->
-            <div class="form-group col-lg-8">
-              <label>Nombre / Razon Social</label>
+            <div class="form-group col-lg-6">
+              <label>Descripción</label>
               <input type="" name="" class="form-control" id="" value="">
             </div>
             <div class="form-group col-lg-4">
-              <label>RFC</label>
-              <input type="rfc" name="" class="form-control" id="" value="">
-            </div>
-            <div class="form-group col-lg-8">
-              <label>EMAIL</label>
+              <label>Dias de madurez</label>
               <input type="" name="" class="form-control" id="" value="">
+            </div>        
+            <div class="modal-footer ">
+              <button type="submit" class="btn btn-primary " style="width: 30%;" >Guardar</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal" style="width: 30%;" >Cerrar</button>
             </div>
-            <!-- DIRECCION -->
-            <div class="form-group col-lg-12">
-              <label>Direccion</label>
-            </div>
-            <!-- CODIGO POSTAL -->
-            <div class="form-group col-lg-2">
-              <label>Codigo Postal</label>
-              <input type="uso" name="" class="form-control" id="" value="">
-            </div>
-            <!--CALLE -->
-            <div class="form-group col-lg-2">
-              <label>Calle</label>
-              <input type="uso" name="" class="form-control" id="" value="">
-            </div>
-            <!-- NUMERO INTERIOR -->
-            <div class="form-group col-lg-2">
-              <label>Numero Interior</label>
-              <input type="uso" name="" class="form-control" id="" value="">
-            </div>
-            <!--NUMERO EXTERIOR -->
-            <div class="form-group col-lg-2">
-              <label>Numero Exterior</label>
-              <input type="uso" name="" class="form-control" id="" value="">
-            </div>
-            <!--COLONIA -->
-            <div class="form-group col-lg-6">
-              <label>Colonia</label>
-              <input type="uso" name="" class="form-control" id="" value="">
-            </div>
-            <!--LOCALIDAD -->
-            <div class="form-group col-lg-2">
-              <label>Localidad</label>
-              <input type="uso" name="" class="form-control" id="" value="">
-            </div>
-            <!--ESTADO -->
-            <div class="form-group col-lg-2">
-              <label>Estado</label>
-              <input type="uso" name="" class="form-control" id="" value="">
-            </div>
-            <!--PAIS -->                
-            <div class="form-group col-lg-2">
-              <label>Pais</label>
-              <input type="" name="" class="form-control" id="" value="">
-            </div> 
-            <!--COMENTARIOS -->
-            <div class="form-group col-lg-10">
-              <label>Comentarios</label>
-              <textarea rows="3" cols="50" type="" name="" class="form-control" id="" value="">
-              </textarea>  
-            </div>  
+          </div>
+        <!-- /.modal-content --> 
+        </div>
+        <!-- /.modal-dialog --> 
+      </div>
     </div>
   </div>
-</div>
