06d632ba3b4209a2483a9b98727159e2a982ba04
julio <julioingsist@gmail.com>
2017-12-01 00:18:55 -0700

    se generan seed faltantes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsuariosAdmin::class);
-        $this->call(Bodegas::class);
+        $this->call(Clientes::class);
         $this->call(Clasificaciones::class);
+        $this->call(Bodegas::class);
+        $this->call(Ubicaciones::class);
     }
 }
