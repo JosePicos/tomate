5e5288ade61145042e02cee678eaa6c6d19b63d3
julio <julioingsist@gmail.com>
2017-12-01 23:03:19 -0700

    se termina migracion de domicilios de clientes

--- database/migrations/2017_12_01_065856_create_domicilios_clientes_table.php
+++ database/migrations/2017_12_01_065856_create_domicilios_clientes_table.php
@@ -13,7 +13,19 @@ class CreateDomiciliosClientesTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('domicilios_clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_cliente')->unsigned();
+            $table->foreign('id_cliente')->references('id')->on('cliente')->onDelete('restrict');
+            $table->string('pais', 50);
+            $table->string('estado', 50);
+            $table->string('municipio', 50);
+            $table->string('colonia', 50);
+            $table->string('codigo_postal', 10);
+            $table->string('calle', 50);
+            $table->string('numero', 10);    
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +35,6 @@ class CreateDomiciliosClientesTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('domicilios_clientes');
     }
 }
