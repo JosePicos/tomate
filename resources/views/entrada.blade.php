49121966c178710e01164074cb2ab4032ddc8094
Jose Picos <jl.moonlight93@gmail.com>
2017-12-06 16:07:52 -0700

    Se crea la vista de entrada

--- resources/views/entrada.blade.php
+++ resources/views/entrada.blade.php
@@ -1,15 +1,47 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row">
-         <div class="container-fluid">
-        <div class="col-md-12 col-md-offset dark-grey">
-            <div class="panel panel-default">
-                <div class="panel-heading">Entrada de Lotes</div>
-                  
+<div class="col-md-12 col-md-offset dark-grey">
+      <div class="panel-body">
+        <div class="container-fluid">
+        <section class="container">
+          <div class="container-page">        
+            <div class="col-md-12">
+              <h3 class="dark-grey">Entada de Lotes</h3>
+                  <div class="form-group col-lg-1">
+                    <label></label>
+                    <button type="submit" class=" form-control btn btn-primary" data-toggle="modal" data-target="#buscarprod"><span class="glyphicon glyphicon-search"></span></button>
+                  </div>
+                  <div class="form-group col-lg-2">
+                    <label>ID Producto</label>
+                    <input type="" name="" class="form-control" id="" value="">
+                  </div>
+                  <div class="form-group col-lg-6">
+                    <label>Descipción</label>
+                    <input type="" name="" class="form-control" id="" value="">
+                  </div>
+                  <div class="form-group col-lg-2">
+                    <label>Feccha de corte</label>
+                    <input type="date" name="" class="form-control" id="" value="">
+                  </div>
+                  <div class="form-group col-lg-2">
+                    <label>Clasificación</label>
+                      <select type="uso" name="" class="form-control" id="" value="" >
+                        <option value="Tacos" >Normal</option>
+                        <option>Urgente</option>
+                    </select>
+                  </div>
+                  <div class="form-group col-lg-4">
+                    <label>Bodega</label>
+                    <input type="" name="" class="form-control" id="" value="">
+                  </div>
+                  <div class="form-group col-lg-4">
+                    <label>Ubicación</label>
+                    <input type="" name="" class="form-control" id="" value="">
+                  </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@include('productos.buscador');
 @endsection
