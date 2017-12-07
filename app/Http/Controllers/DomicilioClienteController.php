83032af33059fe987695e0026c0b6ad98921c36a
Julio César Castro Beltrán <julioingsist@gmail.com>
2017-12-07 00:36:30 -0700

    se termina vista de domicilios de clientes

--- app/Http/Controllers/DomicilioClienteController.php
+++ app/Http/Controllers/DomicilioClienteController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Pais;
 use App\Cliente;
-use App\DomicilioCliente;
 use Auth;
+use Illuminate\Support\Facades\DB;
 
 class DomicilioClienteController extends Controller
 {
@@ -15,7 +15,13 @@ class DomicilioClienteController extends Controller
 	{
 		$paises = Pais::all();
 		$cliente = Cliente::where('id_user', Auth::user()->id)->first();
-		$domicilios = DomicilioCliente::where('id_cliente', $cliente->id);
+		$domicilios = DB::Table('domicilios_clientes')
+                    ->join('paises', 'domicilios_clientes.pais', '=', 'paises.id')
+                    ->select('domicilios_clientes.id', 'domicilios_clientes.codigo_postal',
+                             'paises.nombre as pais', 'domicilios_clientes.estado', 
+                    	     'domicilios_clientes.colonia', 'domicilios_clientes.numero')
+                    ->where('domicilios_clientes.id_cliente', $cliente->id)
+                    ->paginate(10);
 
 		return view('direccionCliente', compact('paises', 'domicilios'));
 	}
