f330026e60d904e37d98fe1e2d0fd37d1e9128a2
Julio César Castro Beltrán <julioingsist@gmail.com>
2017-12-07 23:22:05 -0700

    se valida nombre del usuario en el registro

--- public/js/formularios.js
+++ public/js/formularios.js
@@ -1,29 +1,20 @@
-function validaCodigoPostal(obj){
-  var codigoPostal = obj.value;
-  var validaCodigo = new RegExp('([0-9]{5})');
-  var matchArray = codigoPostal.match(validaCodigo);
-  if (matchArray == null){
-    alert('Error: el campo Codigo Postal debe estar formado por 5 dígitos numéricos');
-    obj.value = "";
-  }
-}
-
-function validaNombre(e){
+function validarNombre(e)
+{
   key = e.keyCode || e.which;
   tecla = String.fromCharCode(key).toLowerCase();
   letras = " áéíóúabcdefghijklmnñopqrstuvwxyz";
   especiales = [8,46,13];
 
   tecla_especial = false;
-  for(var i in especiales){
-    if(key == especiales[i]){
-      tecla_especial = true;
-      break;
+  for (var i in especiales) {
+    if (key == especiales[i]) {
+       tecla_especial = true;
+       break;
     }
   }
 
-  if(letras.indexOf(tecla)==-1 && !tecla_especial){
-    return false;
+  if(letras.indexOf(tecla)==-1 && !tecla_especial) {
+     return false;
   }
 }
 
@@ -32,8 +23,8 @@ function validarFecha(obj)
   var fecha = new Date(obj.value);
   var fechaActual = new Date();
   
-  if (fecha>fechaActual) {
-    obj.value = "";
-    alert('La fecha de corte no puede ser mayor a la fecha actual');
+  if (fecha > fechaActual) {
+      obj.value = "";
+      alert('La fecha de corte no puede ser mayor a la fecha actual');
   } 
 }
